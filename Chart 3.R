02d0693710bcf66adeac5d3232901ae57c4908e0
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-15 00:25:39 -0700

    Updated

--- Chart 3.R
+++ Chart 3.R
@@ -1,23 +1,14 @@
+library(tidyverse)
+library(dplyr)
+
 # Load ggplot2
 library(ggplot2)
 
-# evaluates possible consumers vs revenue when there is inclusivity of shades
+
+shades <- read.csv("https://github.com/the-pudding/data/blob/725dc18a59a055891dee25e85652f416b0065bdd/makeup-shades/shades.csv")
+
+# evaluates possible consumers vs revenue when there is exclusivity of shades
 # between Fenty and Mac Cosmetics!
-
-Consum_vs_revenue <- data.frame()
-
-
-# Grouped Bar Plot
-ggplot(data = midwest) +
-  geom_bar(Consum_vs_revenue, aes(
-    x = Consumers,
-    y = Revenue))
-
-
-ggplot(data = midwest) +
-  geom_col(mapping = aes(
-    x = state,
-    y = poptotal,
-    color = state))
+comp_rev <- data.frame()
 
 
