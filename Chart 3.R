1738cc01d5248e552e778f186a2d3ef80d06a9fc
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-15 20:28:14 -0700

    Updated!

--- Chart 3.R
+++ Chart 3.R
@@ -1,19 +1,17 @@
-library(tidyverse)
-library(dplyr)
-
 # Load ggplot2
 library(ggplot2)
 
-title = " Revenue while Shade increases over Years"
 
 foundation_dataset <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/makeup-shades/shades.csv")
 
-# evaluates possible consumers vs revenue when there is exclusivity of shades
-# between Fenty and Mac Cosmetics!
 
-fenty_data <- subset(foundation_dataset, brand == "Fenty")
-
+# Plotting Bar
 ggplot(data = foundation_dataset) + 
-  geom_bar(mapping = aes(x = product, y = product, fill = "Fenty"), stat = "identity")
+  geom_bar(mapping = aes(
+    x = product,
+    y = product, 
+    fill = "Fenty"), 
+    stat = "identity") + 
+  ggtitle("Revenue When Shades Options Increase Over The Years")
 
   
