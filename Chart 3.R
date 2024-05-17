0a41bbbcbbf185a525fd8ddc070bb444b8b54064
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-16 23:22:18 -0700

    Updated chart 3

--- Chart 3.R
+++ Chart 3.R
@@ -4,20 +4,16 @@ library(ggplot2)
 foundation_dataset <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/makeup-shades/shades.csv")
 
 #filtering data just to get the two brands!
+ 
+filtered_data <- subset(foundation_dataset, brand %in% c("Fenty", "MAC"))
 
-filtered_data <- subset(foundation_dataset, brand %in% c("House of Tara", "Estée Lauder"))
+# Bar graph: Comparing the difference saturation levels between Fenty and Mac. 
 
-# Bar graph
-ggplot(data = filtered_data) + 
-  geom_bar(mapping = aes(
-    x = product,
-    y = brand, 
-    fill = "identification"), 
-    stat = "identity") + 
-  ggtitle("Nigerain Based .Vs. American Based")
+ggplot(data = filtered_data, aes(
+  x = product, 
+  y = S, 
+  fill = brand)) + 
+  geom_bar(stat = "identity",position = "dodge", colour = "black") + 
+  labs(y = "Saturation Levels", x = "Brands", fill = "Brands") + 
+  ggtitle("Fenty vs. Mac: Saturation Level Differences")
 
-
-
-
-
-  
