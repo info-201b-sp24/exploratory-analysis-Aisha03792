a3192595e16b75db6c764f60d517e0a777b1b047
GiselleCal <gcalva@uw.edu>
2024-05-18 20:51:53 -0700

    Update changes

--- Summary Calculation.R
+++ Summary Calculation.R
@@ -1,31 +1,25 @@
 # A function that takes in a dataset and returns a list of info about it:
 
-summary_info_one <- list(summary_info_some_max_value)
-summary_info_num_observations_one <- nrow(fenty_x_mac_table)
-summary_info_some_max_value <- fenty_x_mac_table %>%
+summary_info_num_observations_Lightness <- nrow(fenty_x_mac_table)
+summary_info_lightness_min_value <- fenty_x_mac_table %>%
   filter(Lightness == min(Lightness, na.rm = T)) %>%
-  select(Brand)
-
-summary_info_two<- list()
-summary_info_num_observations_two <- nrow(fenty_x_mac_table)
-summary_info_some_max_value <- fenty_x_mac_table %>%
+  select(Brand, Lightness)
+summary_info_lightness_max_value <- fenty_x_mac_table %>%
   filter(Lightness == max(Lightness, na.rm = T)) %>%
-  select(Brand)
+  select(Brand, Lightness)
 
-summary_info_three <- list()
-summary_info$num_observations_three <- nrow(fenty_x_table)
-summary_info$some_max_value <- fenty_x_table %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
+summary_info_num_observations_Saturation <- nrow(fenty_x_mac_table)
+summary_info_saturation_max_value <- fenty_x_mac_table %>%
+  filter(Saturation == max(Saturation, na.rm = T)) %>%
+  select(Brand, Saturation)
 
-summary_info_four <- list()
-summary_info$num_observations_four <- nrow(fenty_x_table)
-summary_info$some_max_value <- fenty_x_table %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
+summary_info_num_observations_Hue <- nrow(fenty_x_mac_table)
+summary_info_Hue_Fenty <- fenty_table %>%
+  select(H)
+Unique_Fenty_Hues <- unique(summary_info_Hue_Fenty)
 
-summary_info_five <- list()
-summary_info$num_observations_five <- nrow(fenty_x_table)
-summary_info$some_max_value <- fenty_x_table %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
+summary_info_Hue_MAC <- mac_table %>%
+  select(H)
+Unique_MAC_Hues <- unique(summary_info_Hue_MAC)
+
+summary_info <- list(summary_info_lightness_min_value, summary_info_lightness_max_value, summary_info_saturation_max_value, Unique_Fenty_Hues, Unique_MAC_Hues)
